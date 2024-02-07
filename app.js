31455fc1f250cbff733961762e831d1c3616eb2c
syeef <syeefislam@yahoo.com>
2024-02-07 07:18:07 +0530

    config for database added

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config();
+const sequelize = require("./util/database");
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,6 +13,18 @@ app.use((req, res) => {
   res.sendFile(path.join(__dirname, `./public/${req.url}`));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is runing on ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is runing on ${PORT}`);
+// });
+
+async function startServer() {
+  try {
+    await sequelize.sync();
+    //await sequelize.sync({force:true});
+    app.listen(process.env.PORT || 3000);
+    console.log("Server is running...");
+  } catch (error) {
+    console.error("Error syncing database:", error);
+  }
+}
+startServer();
