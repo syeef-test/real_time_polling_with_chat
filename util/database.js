c3c84f492c3f9c1a18f5d39c0326819f6af5a850
syeef <syeefislam@yahoo.com>
2024-02-09 15:04:10 +0530

    database code in util folder chnaged so it will work on remote,for frontend please give remote address on dashboard.html at line 707

--- util/database.js
+++ util/database.js
@@ -3,11 +3,23 @@ dotenv.config();
 
 const Sequelize = require("sequelize");
 
+//Woork on local pc
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   { dialect: "mysql", host: "127.0.0.1" }
+// );
+
+//Work on remote
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
-  { dialect: "mysql", host: "127.0.0.1" }
+  {
+    dialect: process.env.DB_TYPE,
+    host: process.env.DB_HOST,
+  }
 );
 
 module.exports = sequelize;
